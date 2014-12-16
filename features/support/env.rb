1df9c15a1027d2f2da63eabc72706b8b2535e145
jclary1070 <jclary@gci-jclary-m.ini.us.ad.gannett.com>
2014-12-16 11:20:00 -0500

    created the necessary folder structure and files for capybara/cucumber. updated env.rb file with necessary configuration

--- features/support/env.rb
+++ features/support/env.rb
@@ -2,4 +2,9 @@ require 'rspec/expectations'
 require 'capybara/cucumber'
 require 'selenium-webdriver'
 require 'timeout'
-require 'rails_helper'
+
+
+# Using the Selenium driver as default so we can pop-open Firefox and get a
+# good warm fuzzy feeling
+     
+Capybara.default_driver = :selenium
