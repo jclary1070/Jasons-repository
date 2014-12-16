420b2da2ec2f76b228f8a70309a4007edf6d293c
jclary1070 <jclary@gci-jclary-m.ini.us.ad.gannett.com>
2014-12-16 10:42:21 -0500

    created the necessary folder structure and files for capybara/cucumber. updated env.rb file with necessary configuration

---
+++ features/step_definitions/my_feature_steps.rb
@@ -0,0 +1,7 @@
+When(/^I visit the Dominos home page$/) do
+  visit('https://orders.dominos.com')
+end
+ 
+Then(/^I should see the Dominos home page$/) do
+  pending # express the regexp above with the code you wish you had
+end
