9366e6ba9b0eb73eb60ebf9ebc6d2c83759fdf5d
jclary <jclary@gci-jclary-m.ini.us.ad.gannett.com>
2015-01-09 15:50:26 -0500

    added gherkin to .feature and wrote step definition code for Order Dominos Pizza Online

--- features/step_definitions/Steps.rb
+++ features/step_definitions/Steps.rb
@@ -4,3 +4,12 @@ end
 Then(/^I should see the Dominos Location Search page$/) do
   expect(page).to have_selector(:id, 'locationsSearchPage')
 end
+When(/^I select the Dominos Location Search page$/) do
+	I should be taken to Order_Settings on the homepage
+end
+Then(/^I should see 'Address_Type_Select'/) do
+	street = "123 Main Street"
+	City = "Carmel"
+	State = "IN"
+	Zip Code = '46032'
+end
--- features/step_definitions/visit the Dominos home page$: do.rb
+++ features/step_definitions/visit the Dominos home page$: do.rb
@@ -7,3 +7,10 @@ end
 Then /^I should go to Order_Settings on the home page$/ do
 	 expect(page).to have_selector(:id, 'homePage')
 end
+When /^I go to the "Address_Type_Select" on the homepage$/ do 
+ address = 'Address_Type_Select'
+ Street = "123 Main Street"
+ City = "Carmel"
+ State = "IN"
+ Zip Code = '46032'
+end
